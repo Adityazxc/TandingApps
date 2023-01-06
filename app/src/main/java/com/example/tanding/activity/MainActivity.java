bdb653da54a69d21bc939c27ea6e06e23c17d210
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2023-01-06 17:15:27 +0700

    Home components

--- app/src/main/java/com/example/tanding/activity/MainActivity.java
+++ app/src/main/java/com/example/tanding/activity/MainActivity.java
@@ -3,13 +3,36 @@ package com.example.tanding.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import com.example.tanding.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialToolbar materialToolbar;
+    private SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        materialToolbar = findViewById(R.id.appBarLayout);
+        searchView = findViewById(R.id.search_view);
+
+
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
